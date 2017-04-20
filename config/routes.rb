c29d9ac1c91462d256642b02561d1d0d3024716d
unknown <BBSYDP-CITY@CCL1-WS-25.iba.edu.pk>
2017-04-20 13:11:28 +0500

    articles

--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,11 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+Rails.application.routes.draw do
+  get 'welcome/index'
+ 
+  root 'welcome#index'
+end
+
+
+
